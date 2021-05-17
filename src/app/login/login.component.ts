64135caf805426aad7cdb00b80bc5ed93ab2874e
Rudra <rudraraman1698@gmail.com>
2021-05-17 22:44:28 +0530

    routing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,52 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-aim="Your Perfect Banking Partner"
+  aim = "Your Perfect Banking Partner"
 
-accno="Account Number Please"
+  accno = "Account Number Please"
 
-pswd="";
+  pswd = "";
 
-AccountDetails:any = {
-  1000: { acno: 1000,  username: "userone", password: "userone", balance: 50000 },
-  1001: { acno: 1001, username: "usertwo", password: "usertwo", balance: 5000 },
-  1002: { acno: 1002,  username: "userthree", password: "userthree", balance: 10000 },
-  1003: { acno: 1003,  username: "userfour", password: "userfour", balance: 6000 }
-}
+  AccountDetails: any = {
+    1000: { acno: 1000, username: "userone", password: "userone", balance: 50000 },
+    1001: { acno: 1001, username: "usertwo", password: "usertwo", balance: 5000 },
+    1002: { acno: 1002, username: "userthree", password: "userthree", balance: 10000 },
+    1003: { acno: 1003, username: "userfour", password: "userfour", balance: 6000 }
+  }
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-accnoChange(event:any){
-  this.accno=event.target.value
-  console.log(this.accno);
-  
-}
-pswdChange(event:any){
-  this.pswd=event.target.value;
-  console.log(this.pswd);
-}
-// }
-login(){
+  accnoChange(event: any) {
+    this.accno = event.target.value
+    console.log(this.accno);
 
-var acno=this.accno;
-var pswd=this.pswd;
-let users = this.AccountDetails
-        if (acno in users) {
-            if (pswd == users[acno]["password"]) {
-                
-                alert("Login Successful")
-            }
-            else {
-              alert("Incorrect Password")
-            }
-        }
-        else {
-          alert("Invalid Account")
-        }
+  }
+  pswdChange(event: any) {
+    this.pswd = event.target.value;
+    console.log(this.pswd);
+  }
+  // }
+  login() {
+
+    var acno = this.accno;
+    var pswd = this.pswd;
+    let users = this.AccountDetails
+    if (acno in users) {
+      if (pswd == users[acno]["password"]) {
+
+        alert("Login Successful")
+        this.router.navigateByUrl("dashboard")
+      }
+      else {
+        alert("Incorrect Password")
+      }
+    }
+    else {
+      alert("Invalid Account")
+    }
 
 
-}
+  }
+  register() {
+    this.router.navigateByUrl("register")
+  }
 }
